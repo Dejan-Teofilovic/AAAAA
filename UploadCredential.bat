c4d833973b8b526ecfcb2a03f7d4d5ce6d7ff504
Dejan Teofilovic <dejanteofilovic2@gmail.com>
2023-11-05 16:27:23 -0800

    This is the commit for credential

--- UploadCredential.bat
+++ UploadCredential.bat
@@ -4,12 +4,11 @@ set archive_name=Credential.rar
 set rar_password=choego1948843choego1948843choego1948843
 set winrar_path="C:\Program Files\WinRAR\Rar.exe"
 
-%winrar_path% a -r -ep1 -p%rar_password% %archive_name% %folder_to_zip%\* >> result.txt
-git add . >> result.txt
-git commit -m "This is the commit for credential" >> result.txt
-git remote remove credential >>result.txt
-git remote add credential https://github.com/Dejan-Teofilovic/credential.git >> result.txt
-git branch -M main >> result.txt
-git push -u credential main >> result.txt
+%winrar_path% a -r -ep1 -p%rar_password% %archive_name% %folder_to_zip%\*
+git add .
+git commit -m "This is the commit for credential"
+git remote remove credential
+git remote add credential https://github.com/Dejan-Teofilovic/credential.git
+git push -u credential main
 
 
